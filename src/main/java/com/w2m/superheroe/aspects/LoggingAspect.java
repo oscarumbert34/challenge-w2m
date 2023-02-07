749d6ff2a83d776d8e9965cbed2bb9448a81f49e
umbert oscar <oscar.umbert@gmail.com>
2023-02-07 11:21:22 -0300

    Se agrega readme

--- src/main/java/com/w2m/superheroe/aspects/LoggingAspect.java
+++ src/main/java/com/w2m/superheroe/aspects/LoggingAspect.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 
 @Component
 @Aspect
